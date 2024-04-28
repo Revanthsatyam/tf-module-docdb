1de6c2d7693b81e720495470150938c22bf84ee0
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-28 06:55:05 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_docdb_subnet_group" "main" {
-  name       = "${local.name_prefix}-subnet"
+  name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
-  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet" })
+  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
 
 resource "aws_security_group" "main" {
@@ -27,15 +27,25 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = var.engine_family
+  name        = "${local.name_prefix}-pg"
+  description = "${local.name_prefix}-pg"
+  tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
+}
+
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${local.name_prefix}-cluster"
-  engine                  = var.engine
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = var.backup_retention_period
-  preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = var.skip_final_snapshot
-  vpc_security_group_ids  = [aws_security_group.main.id]
-  db_subnet_group_name    = aws_docdb_subnet_group.main.id
+  cluster_identifier              = "${local.name_prefix}-cluster"
+  engine                          = "docdb"
+  master_username                 = data.aws_ssm_parameter.master_username.value
+  master_password                 = data.aws_ssm_parameter.master_password.value
+  backup_retention_period         = var.backup_retention_period
+  preferred_backup_window         = var.preferred_backup_window
+  skip_final_snapshot             = var.skip_final_snapshot
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids          = [aws_security_group.main.id]
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+  engine_version                  = var.engine_version
 }
 
--- vars.tf
+++ vars.tf
@@ -7,3 +7,5 @@ variable "engine" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
+variable "engine_family" {}
+variable "engine_version" {}
