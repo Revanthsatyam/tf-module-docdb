019438bf2619aea673f4c2027b2b111e0ee02806
Revanthsatyam <satyamrevanth@gmail.com>
2024-04-28 06:17:12 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_docdb_subnet_group" "main" {
-  name       = "${local.name_prefix}-subnet-group"
+  name       = "${local.name_prefix}-subnet"
   subnet_ids = var.subnet_ids
-  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
+  tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet" })
 }
 
 resource "aws_security_group" "main" {
@@ -27,34 +27,15 @@ resource "aws_security_group" "main" {
   }
 }
 
-resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = var.engine_family
-  name        = "${local.name_prefix}-pg"
-  description = "${local.name_prefix}-pg"
-  tags        = merge(local.tags, { Name = "${local.name_prefix}-pg" })
-}
-
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier              = "${local.name_prefix}-cluster"
-  engine                          = "docdb"
-  master_username                 = data.aws_ssm_parameter.master_username.value
-  master_password                 = data.aws_ssm_parameter.master_password.value
-  backup_retention_period         = var.backup_retention_period
-  preferred_backup_window         = var.preferred_backup_window
-  skip_final_snapshot             = var.skip_final_snapshot
-  db_subnet_group_name            = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids          = [aws_security_group.main.id]
-  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
-  tags                            = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
-  engine_version                  = var.engine_version
-  storage_encrypted               = true
-  kms_key_id                      = var.kms_key_id
-}
-
-resource "aws_docdb_cluster_instance" "main" {
-  count              = var.instance_count
-  identifier         = "${local.name_prefix}-cluster-instance-${count.index+1}"
-  cluster_identifier = aws_docdb_cluster.main.id
-  instance_class     = var.instance_class
+  cluster_identifier      = "${local.name_prefix}-cluster"
+  engine                  = var.engine
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
+  vpc_security_group_ids  = [aws_security_group.main.id]
+  db_subnet_group_name    = aws_docdb_subnet_group.main.id
 }
 
--- vars.tf
+++ vars.tf
@@ -1,13 +1,9 @@
+variable "tags" {}
 variable "env" {}
 variable "subnet_ids" {}
-variable "tags" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "engine" {}
 variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "skip_final_snapshot" {}
-variable "vpc_id" {}
-variable "sg_ingress_cidr" {}
-variable "engine_version" {}
-variable "engine_family" {}
-variable "instance_count" {}
-variable "instance_class" {}
-variable "kms_key_id" {}
